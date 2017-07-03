57dbadb01053d5adb860d04b692f57ceb2fb3714
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-04 00:03:41 +0200

    Adds JSON-RPC response types.

---
+++ src/RpcResponse.ts
@@ -0,0 +1,67 @@
+/**
+ * JSON-RPC response.
+ * @param T Type of the `result` property.
+ */
+export interface RpcResponse<T = any> {
+  /**
+   * Result of the invoked method, if successful.
+   */
+  readonly result: T | null
+
+  /**
+   * Error object, if method invocation failed.
+   */
+  readonly error: Error | null
+
+  /**
+   * Identifier of the request associated with the response, if any.
+   */
+  readonly id: number | string | null
+}
+
+/**
+ * Type of the `error` property of [[RpcResponse]].
+ */
+export interface Error {
+  /**
+   * Error code.
+   *
+   * See [[ErrorCode]] for a list of predefined error codes.
+   */
+  readonly code: number
+
+  /**
+   * Error description.
+   */
+  readonly message: string
+}
+
+/**
+ * JSON-RPC 2.0 predefined error codes.
+ */
+export enum ErrorCode {
+  /**
+   * The request body is an invalid JSON-RPC request.
+   */
+  InvalidRequest = -32600,
+
+  /**
+   * The invoked method does not exist.
+   */
+  MethodNotFound = -32601,
+
+  /**
+   * The method arguments are invalid.
+   */
+  InvalidParams = -32602,
+
+  /**
+   * Internal JSON-RPC error.
+   */
+  InternalError = -32603,
+
+  /**
+   * The request body is an invalid JSON document.
+   */
+  ParseError = -32700
+}
