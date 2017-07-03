03bd738729c4104b92e1e41769c9eec3ef09400b
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-03 17:49:13 +0200

    Adds JSON-RPC request type.

---
+++ src/RpcRequest.ts
@@ -0,0 +1,20 @@
+/**
+ * JSON-RPC request.
+ * @param T Type of the `params` property.
+ */
+export interface RpcRequest<T extends any[] | undefined> {
+  /**
+   * Name of the method to invoke.
+   */
+  readonly method: string
+
+  /**
+   * List of arguments to invoke the method with, if any.
+   */
+  readonly params?: T
+
+  /**
+   * Request identifier.
+   */
+  readonly id: number | string | null
+}
