a98b499e475343ade4449456fc578b57ad676952
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:16:42 +0200

    Adds unit test for the Revoke command.

--- test/Commands/Revoke.ts
+++ test/Commands/Revoke.ts
@@ -4,5 +4,41 @@ import { TestCommand } from '../test-helpers'
 describe('/Revoke()', function() {
   it(
     'should return a properly configured JSON-RPC request for the revoke API command',
+    function() {
+      const to = 'address2'
+      const permissions = 'connect,send,receive,issue,mine,admin,activate'
+      const quantity = 10
+      const nativeAmount = 10
+      const comment = 'comment'
+      const recipient = 'recipient'
+
+      // Signature 1: [string, string, number]
+      TestCommand(
+        Revoke(to, permissions, quantity),
+        Revoke,
+        [to, permissions, quantity],
+      )
+
+      // Signature 2: [string, string, number, number]
+      TestCommand(
+        Revoke(to, permissions, quantity, nativeAmount),
+        Revoke,
+        [to, permissions, quantity, nativeAmount],
+      )
+
+      // Signature 3: [string, string, number, number, string]
+      TestCommand(
+        Revoke(to, permissions, quantity, nativeAmount, comment),
+        Revoke,
+        [to, permissions, quantity, nativeAmount, comment],
+      )
+
+      // Signature 4: [string, string, number, number, string, string]
+      TestCommand(
+        Revoke(to, permissions, quantity, nativeAmount, comment, recipient),
+        Revoke,
+        [to, permissions, quantity, nativeAmount, comment, recipient],
+      )
+    },
   )
 })
