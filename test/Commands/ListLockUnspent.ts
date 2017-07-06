224374da01bf7acd3448e4b40acc115f1d1002e0
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:11:56 +0200

    Adds unit test for the ListLockUnspent command.

--- test/Commands/ListLockUnspent.ts
+++ test/Commands/ListLockUnspent.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/ListLockUnspent()', function() {
   it(
     'should return a properly configured JSON-RPC request for the listlockunspent API command',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        ListLockUnspent(),
+        ListLockUnspent,
+      )
+    },
   )
 })
