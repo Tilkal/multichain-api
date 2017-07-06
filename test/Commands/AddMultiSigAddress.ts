65661a42acf4a6aa37ceebb21defce7832aac8b1
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:04:59 +0200

    Adds unit test for the AddMultiSigAddress command.

--- test/Commands/AddMultiSigAddress.ts
+++ test/Commands/AddMultiSigAddress.ts
@@ -4,5 +4,24 @@ import { TestCommand } from '../test-helpers'
 describe('/AddMultiSigAddress()', function() {
   it(
     'should return a properly configured JSON-RPC request for the addmultisigaddress API command',
+    function() {
+      const requiredSignatures = 2
+      const keys = ['key1', 'key2', 'key3']
+      const account = 'account'
+
+      // Signature 1: [number, string[]]
+      TestCommand(
+        AddMultiSigAddress(requiredSignatures, keys),
+        AddMultiSigAddress,
+        [requiredSignatures, keys],
+      )
+
+      // Signature 2: [number, string[], string]
+      TestCommand(
+        AddMultiSigAddress(requiredSignatures, keys, account),
+        AddMultiSigAddress,
+        [requiredSignatures, keys, account],
+      )
+    },
   )
 })
