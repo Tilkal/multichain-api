023558a7a7a30777e34dad58490a007adaa343f6
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:10:54 +0200

    Adds unit test for the ImportWallet command.

--- test/Commands/ImportWallet.ts
+++ test/Commands/ImportWallet.ts
@@ -4,5 +4,15 @@ import { TestCommand } from '../test-helpers'
 describe('/ImportWallet()', function() {
   it(
     'should return a properly configured JSON-RPC request for the importwallet API command',
+    function() {
+      const filename = 'filename'
+
+      // Signature 1: [string]
+      TestCommand(
+        ImportWallet(filename),
+        ImportWallet,
+        [filename],
+      )
+    },
   )
 })
