6b99f7910001260dacb259a5116998d40105e3cf
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:09:45 +0200

    Adds unit test for the GetWalletInfo command.

--- test/Commands/GetWalletInfo.ts
+++ test/Commands/GetWalletInfo.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/GetWalletInfo()', function() {
   it(
     'should return a properly configured JSON-RPC request for the getwalletinfo API command',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        GetWalletInfo(),
+        GetWalletInfo,
+      )
+    },
   )
 })
