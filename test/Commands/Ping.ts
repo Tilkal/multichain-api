ab00f613ba7f849f7b3f96ae90499ca8beb25c51
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:13:59 +0200

    Adds unit test for the Ping command.

--- test/Commands/Ping.ts
+++ test/Commands/Ping.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/Ping()', function() {
   it(
     'should return a properly configured JSON-RPC request for the ping API command',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        Ping(),
+        Ping,
+      )
+    },
   )
 })
