c4ddc47ec5bc308ab043a0b17896d6ae3fe48fe0
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:11:26 +0200

    Adds unit test for the ListAddressTransactions command.

--- test/Commands/ListAddressTransactions.ts
+++ test/Commands/ListAddressTransactions.ts
@@ -4,5 +4,39 @@ import { TestCommand } from '../test-helpers'
 describe('/ListAddressTransactions()', function() {
   it(
     'should return a properly configured JSON-RPC request for the listaddresstransactions API command',
+    function() {
+      const address = 'address'
+      const count = 10
+      const skip = 0
+      const verbose = false
+
+      // Signature 1: [string]
+      TestCommand(
+        ListAddressTransactions(address),
+        ListAddressTransactions,
+        [address],
+      )
+
+      // Signature 2: [string, number]
+      TestCommand(
+        ListAddressTransactions(address, count),
+        ListAddressTransactions,
+        [address, count],
+      )
+
+      // Signature 3: [string, number, number]
+      TestCommand(
+        ListAddressTransactions(address, count, skip),
+        ListAddressTransactions,
+        [address, count, skip],
+      )
+
+      // Signature 4: [string, number, number, boolean]
+      TestCommand(
+        ListAddressTransactions(address, count, skip, verbose),
+        ListAddressTransactions,
+        [address, count, skip, verbose],
+      )
+    },
   )
 })
