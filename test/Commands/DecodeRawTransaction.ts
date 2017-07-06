08bc9731ff42c6d4ece6c56de33f490f2b273938
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:07:59 +0200

    Adds unit test for the DecodeRawTransaction command.

--- test/Commands/DecodeRawTransaction.ts
+++ test/Commands/DecodeRawTransaction.ts
@@ -4,5 +4,15 @@ import { TestCommand } from '../test-helpers'
 describe('/DecodeRawTransaction()', function() {
   it(
     'should return a properly configured JSON-RPC request for the decoderawtransaction API command',
+    function() {
+      const transaction = 'transaction'
+
+      // Signature 1: [string]
+      TestCommand(
+        DecodeRawTransaction(transaction),
+        DecodeRawTransaction,
+        [transaction],
+      )
+    },
   )
 })
