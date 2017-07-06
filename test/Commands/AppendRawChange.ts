c83817e08ff4c37ca20f1383e74964b468e126e0
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:05:28 +0200

    Adds unit test for the AppendRawChange command.

--- test/Commands/AppendRawChange.ts
+++ test/Commands/AppendRawChange.ts
@@ -4,5 +4,24 @@ import { TestCommand } from '../test-helpers'
 describe('/AppendRawChange()', function() {
   it(
     'should return a properly configured JSON-RPC request for the appendrawchange API command',
+    function() {
+      const transaction = 'transaction'
+      const address = 'address'
+      const fee = 1
+
+      // Signature 1: [string, string]
+      TestCommand(
+        AppendRawChange(transaction, address),
+        AppendRawChange,
+        [transaction, address],
+      )
+
+      // Signature 2: [string, string, number]
+      TestCommand(
+        AppendRawChange(transaction, address, fee),
+        AppendRawChange,
+        [transaction, address, fee],
+      )
+    },
   )
 })
