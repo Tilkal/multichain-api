74d481c0b772df41e43f397a1698d913684f679c
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:24:08 +0200

    Adds unit test for the WalletPassphrase command.

--- test/Commands/WalletPassphrase.ts
+++ test/Commands/WalletPassphrase.ts
@@ -4,5 +4,16 @@ import { TestCommand } from '../test-helpers'
 describe('/WalletPassphrase()', function() {
   it(
     'should return a properly configured JSON-RPC request for the walletpassphrase API command',
+    function() {
+      const passphrase = 'passphrase'
+      const timeout = 30
+
+      // Signature 1: [string, number]
+      TestCommand(
+        WalletPassphrase(passphrase, timeout),
+        WalletPassphrase,
+        [passphrase, timeout],
+      )
+    },
   )
 })
