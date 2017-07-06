5c2858a5ab470949aa41400d7e6495604fcb4bd4
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:10:20 +0200

    Adds unit test for the GetWalletTransaction command.

--- test/Commands/GetWalletTransaction.ts
+++ test/Commands/GetWalletTransaction.ts
@@ -4,5 +4,31 @@ import { TestCommand } from '../test-helpers'
 describe('/GetWalletTransaction()', function() {
   it(
     'should return a properly configured JSON-RPC request for the getwallettransaction API command',
+    function() {
+      const transactionId = 'id'
+      const includeWatchOnly = false
+      const verbose = false
+
+      // Signature 1: [string]
+      TestCommand(
+        GetWalletTransaction(transactionId),
+        GetWalletTransaction,
+        [transactionId],
+      )
+
+      // Signature 2: [string, boolean]
+      TestCommand(
+        GetWalletTransaction(transactionId, includeWatchOnly),
+        GetWalletTransaction,
+        [transactionId, includeWatchOnly],
+      )
+
+      // Signature 3: [string, boolean, boolean]
+      TestCommand(
+        GetWalletTransaction(transactionId, includeWatchOnly, verbose),
+        GetWalletTransaction,
+        [transactionId, includeWatchOnly, verbose],
+      )
+    },
   )
 })
