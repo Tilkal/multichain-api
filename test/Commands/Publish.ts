9037e008bbf53031ffe73177cdf38b4110468600
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:14:18 +0200

    Adds unit test for the Publish command.

--- test/Commands/Publish.ts
+++ test/Commands/Publish.ts
@@ -4,5 +4,17 @@ import { TestCommand } from '../test-helpers'
 describe('/Publish()', function() {
   it(
     'should return a properly configured JSON-RPC request for the publish API command',
+    function() {
+      const stream = 'stream'
+      const key = 'key'
+      const data = 'data'
+
+      // Signature 1: [string, string, string]
+      TestCommand(
+        Publish(stream, key, data),
+        Publish,
+        [stream, key, data],
+      )
+    },
   )
 })
