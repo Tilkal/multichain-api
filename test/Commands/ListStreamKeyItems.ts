395b64617b1dd69a37de0a1d5712e481c205ca3e
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:12:54 +0200

    Adds unit test for the ListStreamItems command.

--- test/Commands/ListStreamKeyItems.ts
+++ test/Commands/ListStreamKeyItems.ts
@@ -4,5 +4,48 @@ import { TestCommand } from '../test-helpers'
 describe('/ListStreamKeyItems()', function() {
   it(
     'should return a properly configured JSON-RPC request for the liststreamkeyitems API command',
+    function() {
+      const stream = 'stream'
+      const key = 'key'
+      const verbose = true
+      const count = 10
+      const start = 0
+      const localOrdering = false
+
+      // Signature 1: [string, string]
+      TestCommand(
+        ListStreamKeyItems(stream, key),
+        ListStreamKeyItems,
+        [stream, key],
+      )
+
+      // Signature 2: [string, string, boolean]
+      TestCommand(
+        ListStreamKeyItems(stream, key, verbose),
+        ListStreamKeyItems,
+        [stream, key, verbose],
+      )
+
+      // Signature 3: [string, string, boolean, number]
+      TestCommand(
+        ListStreamKeyItems(stream, key, verbose, count),
+        ListStreamKeyItems,
+        [stream, key, verbose, count],
+      )
+
+      // Signature 4: [string, string, boolean, number, number]
+      TestCommand(
+        ListStreamKeyItems(stream, key, verbose, count, start),
+        ListStreamKeyItems,
+        [stream, key, verbose, count, start],
+      )
+
+      // Signature 5: [string, string, boolean, number, number, boolean]
+      TestCommand(
+        ListStreamKeyItems(stream, key, verbose, count, start, localOrdering),
+        ListStreamKeyItems,
+        [stream, key, verbose, count, start, localOrdering],
+      )
+    },
   )
 })
