dcd936fef69825e472640dde3ec4903ac8fa94dc
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:23:35 +0200

    Adds unit test for the WalletLock command.

--- test/Commands/WalletLock.ts
+++ test/Commands/WalletLock.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/WalletLock()', function() {
   it(
     'should return a properly configured JSON-RPC request for the walletlock API command',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        WalletLock(),
+        WalletLock,
+      )
+    },
   )
 })
