43e49c830aee9ed3b5fbe565f7501ac489231e57
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:16:26 +0200

    Adds unit test for the PublishFrom command.

--- test/Commands/PublishFrom.ts
+++ test/Commands/PublishFrom.ts
@@ -4,5 +4,18 @@ import { TestCommand } from '../test-helpers'
 describe('/PublishFrom()', function() {
   it(
     'should return a properly configured JSON-RPC request for the publishfrom API command',
+    function() {
+      const from = 'address'
+      const stream = 'stream'
+      const key = 'key'
+      const data = 'data'
+
+      // Signature 1: [string, string, string, string]
+      TestCommand(
+        PublishFrom(from, stream, key, data),
+        PublishFrom,
+        [from, stream, key, data],
+      )
+    },
   )
 })
