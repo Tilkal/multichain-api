901ee3760bc5e1904959894957d4560cbb320654
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:06:52 +0200

    Adds unit test for the CreateKeyPairs command.

--- test/Commands/CreateKeyPairs.ts
+++ test/Commands/CreateKeyPairs.ts
@@ -4,5 +4,21 @@ import { TestCommand } from '../test-helpers'
 describe('/CreateKeyPairs()', function() {
   it(
     'should return a properly configured JSON-RPC request for the createkeypairs API command',
+    function() {
+      const count = 1
+
+      // Signature 1: undefined
+      TestCommand(
+        CreateKeyPairs(),
+        CreateKeyPairs,
+      )
+
+      // Signature 2: [string]
+      TestCommand(
+        CreateKeyPairs(count),
+        CreateKeyPairs,
+        [count],
+      )
+    },
   )
 })
