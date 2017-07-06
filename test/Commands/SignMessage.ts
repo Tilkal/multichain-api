118ebe987ea76f7467233e17c51b8cb90c482792
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:19:38 +0200

    Adds unit test for the SignMessage command.

--- test/Commands/SignMessage.ts
+++ test/Commands/SignMessage.ts
@@ -4,5 +4,16 @@ import { TestCommand } from '../test-helpers'
 describe('/SignMessage()', function() {
   it(
     'should return a properly configured JSON-RPC request for the signmessage API command',
+    function() {
+      const address = 'address'
+      const message = 'message'
+
+      // Signature 1: [string, string]
+      TestCommand(
+        SignMessage(address, message),
+        SignMessage,
+        [address, message],
+      )
+    },
   )
 })
