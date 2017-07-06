dc855de3948b06cf2840c581398388fd74f213b4
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:18:20 +0200

    Adds unit test for the SendRawTransaction command.

--- test/Commands/SendRawTransaction.ts
+++ test/Commands/SendRawTransaction.ts
@@ -4,5 +4,23 @@ import { TestCommand } from '../test-helpers'
 describe('/SendRawTransaction()', function() {
   it(
     'should return a properly configured JSON-RPC request for the sendrawtransaction API command',
+    function() {
+      const transaction = 'transaction'
+      const allowHighFees = false
+
+      // Signature 1: [string]
+      TestCommand(
+        SendRawTransaction(transaction),
+        SendRawTransaction,
+        [transaction],
+      )
+
+      // Signature 2: [string, boolean]
+      TestCommand(
+        SendRawTransaction(transaction, allowHighFees),
+        SendRawTransaction,
+        [transaction, allowHighFees],
+      )
+    },
   )
 })
