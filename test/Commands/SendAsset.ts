7cd882fc3a9926b3b972861e625f8954bd845083
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:17:44 +0200

    Adds unit test for the SendAsset command.

--- test/Commands/SendAsset.ts
+++ test/Commands/SendAsset.ts
@@ -4,5 +4,41 @@ import { TestCommand } from '../test-helpers'
 describe('/SendAsset()', function() {
   it(
     'should return a properly configured JSON-RPC request for the sendasset API command',
+    function() {
+      const to = 'address'
+      const asset = 'asset'
+      const quantity = 10
+      const nativeAmount = 10
+      const comment = 'comment'
+      const recipient = 'recipient'
+
+      // Signature 1: [string, string, number]
+      TestCommand(
+        SendAsset(to, asset, quantity),
+        SendAsset,
+        [to, asset, quantity],
+      )
+
+      // Signature 2: [string, string, number, number]
+      TestCommand(
+        SendAsset(to, asset, quantity, nativeAmount),
+        SendAsset,
+        [to, asset, quantity, nativeAmount],
+      )
+
+      // Signature 3: [string, string, number, number, string]
+      TestCommand(
+        SendAsset(to, asset, quantity, nativeAmount, comment),
+        SendAsset,
+        [to, asset, quantity, nativeAmount, comment],
+      )
+
+      // Signature 4: [string, string, number, number, string, string]
+      TestCommand(
+        SendAsset(to, asset, quantity, nativeAmount, comment, recipient),
+        SendAsset,
+        [to, asset, quantity, nativeAmount, comment, recipient],
+      )
+    },
   )
 })
