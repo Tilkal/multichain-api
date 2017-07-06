da1b93e139c7ed52577c7276e17536bf23eafff9
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:08:51 +0200

    Adds unit test for the EncryptWallet command.

--- test/Commands/EncryptWallet.ts
+++ test/Commands/EncryptWallet.ts
@@ -4,5 +4,15 @@ import { TestCommand } from '../test-helpers'
 describe('/EncryptWallet()', function() {
   it(
     'should return a properly configured JSON-RPC request for the encryptwallet API command',
+    function() {
+      const passphrase = 'passphrase'
+
+      // Signature 1: [string]
+      TestCommand(
+        EncryptWallet(passphrase),
+        EncryptWallet,
+        [passphrase],
+      )
+    },
   )
 })
