9f3dc42a7a5fb8584a08b169225a82c636a1d22a
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:09:21 +0200

    Adds unit test for the GetAddresses command.

--- test/Commands/GetAddresses.ts
+++ test/Commands/GetAddresses.ts
@@ -4,5 +4,21 @@ import { TestCommand } from '../test-helpers'
 describe('/GetAddresses()', function() {
   it(
     'should return a properly configured JSON-RPC request for the getaddresses API command',
+    function() {
+      const verbose = false
+
+      // Signature 1: undefined
+      TestCommand(
+        GetAddresses(),
+        GetAddresses,
+      )
+
+      // Signature 2: [boolean]
+      TestCommand(
+        GetAddresses(verbose),
+        GetAddresses,
+        [verbose],
+      )
+    },
   )
 })
