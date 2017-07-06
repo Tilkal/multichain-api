11a92fa5da964977461b589054a50bef940db95b
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:06:04 +0200

    Adds unit test for the ClearMemPool command.

--- test/Commands/ClearMemPool.ts
+++ test/Commands/ClearMemPool.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/ClearMemPool()', function() {
   it(
     'should return a properly configured JSON-RPC request for the clearmempool API command',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        ClearMemPool(),
+        ClearMemPool,
+      )
+    },
   )
 })
