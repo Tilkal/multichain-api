40fdd30f5900ef84de665c9c48484b8f3703f15a
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:07:16 +0200

    Adds unit test for the CreateMultiSig command.

--- test/Commands/CreateMultiSig.ts
+++ test/Commands/CreateMultiSig.ts
@@ -4,5 +4,16 @@ import { TestCommand } from '../test-helpers'
 describe('/CreateMultiSig()', function() {
   it(
     'should return a properly configured JSON-RPC request for the createmultisig API command',
+    function() {
+      const requiredSignatures = 2
+      const keys = ['key1', 'key2', 'key3']
+
+      // Signature 1: [number, string[]]
+      TestCommand(
+        CreateMultiSig(requiredSignatures, keys),
+        CreateMultiSig,
+        [requiredSignatures, keys],
+      )
+    },
   )
 })
