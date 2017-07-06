16c372caba45f1799d160f720453e89a678485e6
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:22:42 +0200

    Adds unit test for the ValidateAddress command.

--- test/Commands/ValidateAddress.ts
+++ test/Commands/ValidateAddress.ts
@@ -4,5 +4,15 @@ import { TestCommand } from '../test-helpers'
 describe('/ValidateAddress()', function() {
   it(
     'should return a properly configured JSON-RPC request for the validateaddress API command',
+    function() {
+      const address = 'address'
+
+      // Signature 1: [string]
+      TestCommand(
+        ValidateAddress(address),
+        ValidateAddress,
+        [address],
+      )
+    },
   )
 })
