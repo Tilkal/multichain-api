f45aadc9f66f8cd0a39b55857ecb2661e575bf8f
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:20:03 +0200

    Adds unit test for the Stop command.

--- test/Commands/Stop.ts
+++ test/Commands/Stop.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/Stop()', function() {
   it(
     'should return a properly configured JSON-RPC request for the stop API command',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        Stop(),
+        Stop,
+      )
+    },
   )
 })
