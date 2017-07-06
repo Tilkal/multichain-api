cc44083bb24a0594f974722815759b7e16ca688e
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:22:00 +0200

    Adds unit test for the Unsubscribe command.

--- test/Commands/Unsubscribe.ts
+++ test/Commands/Unsubscribe.ts
@@ -4,5 +4,23 @@ import { TestCommand } from '../test-helpers'
 describe('/Unsubscribe()', function() {
   it(
     'should return a properly configured JSON-RPC request for the unsubscribe API command',
+    function() {
+      const identifier = 'name'
+      const list = ['name1', 'name2']
+
+      // Signature 1: [string]
+      TestCommand(
+        Unsubscribe(identifier),
+        Unsubscribe,
+        [identifier],
+      )
+
+      // Signature 2: [string[]]
+      TestCommand(
+        Unsubscribe(list),
+        Unsubscribe,
+        [list],
+      )
+    },
   )
 })
