c1cc3298c8d57fc53e9f6107f603718ffd6fa6ea
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:11:42 +0200

    Adds unit test for the ListAssetTransactions command.

--- test/Commands/ListAssetTransactions.ts
+++ test/Commands/ListAssetTransactions.ts
@@ -4,5 +4,47 @@ import { TestCommand } from '../test-helpers'
 describe('/ListAssetTransactions()', function() {
   it(
     'should return a properly configured JSON-RPC request for the listassettransactions API command',
+    function() {
+      const asset = 'asset'
+      const verbose = true
+      const count = 10
+      const start = 0
+      const localOrdering = false
+
+      // Signature 1: [string]
+      TestCommand(
+        ListAssetTransactions(asset),
+        ListAssetTransactions,
+        [asset],
+      )
+
+      // Signature 2: [string, boolean]
+      TestCommand(
+        ListAssetTransactions(asset, verbose),
+        ListAssetTransactions,
+        [asset, verbose],
+      )
+
+      // Signature 3: [string, boolean, number]
+      TestCommand(
+        ListAssetTransactions(asset, verbose, count),
+        ListAssetTransactions,
+        [asset, verbose, count],
+      )
+
+      // Signature 4: [string, boolean, number, number]
+      TestCommand(
+        ListAssetTransactions(asset, verbose, count, start),
+        ListAssetTransactions,
+        [asset, verbose, count, start],
+      )
+
+      // Signature 5: [string, boolean, number, number, boolean]
+      TestCommand(
+        ListAssetTransactions(asset, verbose, count, start, localOrdering),
+        ListAssetTransactions,
+        [asset, verbose, count, start, localOrdering],
+      )
+    },
   )
 })
