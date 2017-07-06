01a63a130f0c2cd04520d40beed65800dcc37152
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:23:08 +0200

    Adds unit test for the VerifyMessage command.

--- test/Commands/VerifyMessage.ts
+++ test/Commands/VerifyMessage.ts
@@ -4,5 +4,17 @@ import { TestCommand } from '../test-helpers'
 describe('/VerifyMessage()', function() {
   it(
     'should return a properly configured JSON-RPC request for the verifymessage API command',
+    function() {
+      const address = 'address'
+      const signature = 'signature'
+      const message = 'message'
+
+      // Signature 1: [string, string, string]
+      TestCommand(
+        VerifyMessage(address, signature, message),
+        VerifyMessage,
+        [address, signature, message],
+      )
+    },
   )
 })
