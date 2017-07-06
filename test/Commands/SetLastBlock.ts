d7024bea3c86d7ba31455c5d2f757ea92e30fd27
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:19:04 +0200

    Adds unit test for the SetLastBlock command.

--- test/Commands/SetLastBlock.ts
+++ test/Commands/SetLastBlock.ts
@@ -4,5 +4,23 @@ import { TestCommand } from '../test-helpers'
 describe('/SetLastBlock()', function() {
   it(
     'should return a properly configured JSON-RPC request for the setlastblock API command',
+    function() {
+      const hash = 'hash'
+      const height = 0
+
+      // Signature 1: [string]
+      TestCommand(
+        SetLastBlock(hash),
+        SetLastBlock,
+        [hash],
+      )
+
+      // Signature 2: [number]
+      TestCommand(
+        SetLastBlock(height),
+        SetLastBlock,
+        [height],
+      )
+    },
   )
 })
