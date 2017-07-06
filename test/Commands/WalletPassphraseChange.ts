76d114887f67ddf400387f6da5b87789f5740123
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:25:13 +0200

    Adds unit test for the WalletPassphraseChange command.

--- test/Commands/WalletPassphraseChange.ts
+++ test/Commands/WalletPassphraseChange.ts
@@ -4,5 +4,16 @@ import { TestCommand } from '../test-helpers'
 describe('/WalletPassphraseChange()', function() {
   it(
     'should return a properly configured JSON-RPC request for the walletpassphrasechange API command',
+    function() {
+      const oldPassphrase = 'old_passphrase'
+      const newPassphrase = 'new_passphrase'
+
+      // Signature 1: [string, string]
+      TestCommand(
+        WalletPassphraseChange(oldPassphrase, newPassphrase),
+        WalletPassphraseChange,
+        [oldPassphrase, newPassphrase],
+      )
+    },
   )
 })
