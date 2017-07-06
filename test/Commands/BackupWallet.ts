38c10363847562ab571712a783167c5fc0b5e77a
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-06 12:05:48 +0200

    Adds unit test for the BackupWallet command.

--- test/Commands/BackupWallet.ts
+++ test/Commands/BackupWallet.ts
@@ -4,5 +4,15 @@ import { TestCommand } from '../test-helpers'
 describe('/BackupWallet()', function() {
   it(
     'should return a properly configured JSON-RPC request for the backupwallet API command',
+    function() {
+      const filename = 'filename'
+
+      // Signature 1: [string]
+      TestCommand(
+        BackupWallet(filename),
+        BackupWallet,
+        [filename],
+      )
+    },
   )
 })
