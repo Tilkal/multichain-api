fec0b9bed59db1a4244ef6d8db9f634a3bb61f47
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-11 14:48:32 +0200

    Adds unit test for the AddNode command.

--- test/Commands/AddNode.ts
+++ test/Commands/AddNode.ts
@@ -4,5 +4,29 @@ import { TestCommand } from '../test-helpers'
 describe('/AddNode', function() {
   it(
     'AddNode() should return a properly configured JSON-RPC request',
+    function() {
+      const host = '192.168.0.1:8570'
+
+      // Signature 1: [string, 'add']
+      TestCommand(
+        AddNode(host, 'add'),
+        AddNode,
+        [host, 'add'],
+      )
+
+      // Signature 2: [string, 'remove']
+      TestCommand(
+        AddNode(host, 'remove'),
+        AddNode,
+        [host, 'remove'],
+      )
+
+      // Signature 3: [string, 'onetry']
+      TestCommand(
+        AddNode(host, 'onetry'),
+        AddNode,
+        [host, 'onetry'],
+      )
+    },
   )
 })
