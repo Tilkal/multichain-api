4cd660f84d28bb90754fe6768c170ed346604c84
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-11 16:29:14 +0200

    Fixes test, account is no longer supported.

--- test/Commands/AddMultiSigAddress.ts
+++ test/Commands/AddMultiSigAddress.ts
@@ -7,20 +7,12 @@ describe('/AddMultiSigAddress', function() {
     function() {
       const requiredSignatures = 2
       const keys = ['key1', 'key2', 'key3']
-      const account = 'account'
 
       // Signature 1: [number, string[]]
       TestCommand(
         AddMultiSigAddress(requiredSignatures, keys),
         AddMultiSigAddress,
         [requiredSignatures, keys],
-      )
-
-      // Signature 2: [number, string[], string]
-      TestCommand(
-        AddMultiSigAddress(requiredSignatures, keys, account),
-        AddMultiSigAddress,
-        [requiredSignatures, keys, account],
       )
     },
   )
