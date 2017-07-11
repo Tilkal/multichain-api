26d08d094c84c24819c7b4f99a392db351111a56
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-11 12:49:43 +0200

    Improves unit test for /main module.

--- test/main.ts
+++ test/main.ts
@@ -1,13 +1,16 @@
 import { expect } from 'chai'
+import * as Commands from '../src/Commands'
 import * as main from '../src/main'
+import { RpcClient } from '../src/RpcClient'
+import { RpcError } from '../src/RpcError'
 
 describe('/main', function() {
   it(
     'should return a module with the correct shape',
     function() {
-      expect(main).to.have.property('Commands').and.to.be.an('object')
-      expect(main).to.have.property('RpcClient').and.to.be.a('function')
-      expect(main).to.have.property('RpcError').and.to.be.a('function')
+      expect(main).to.have.property('Commands', Commands)
+      expect(main).to.have.property('RpcClient', RpcClient)
+      expect(main).to.have.property('RpcError', RpcError)
     },
   )
 })
