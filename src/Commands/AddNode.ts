7dd17025788002dd4b1e2d95df9d53dd961d45ab
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-11 15:53:20 +0200

    Adds type signature and documentation for the AddNode command.

--- src/Commands/AddNode.ts
+++ src/Commands/AddNode.ts
@@ -6,7 +6,7 @@ import { RpcResponse } from '../RpcResponse'
  */
 export interface AddNodeRequest extends RpcRequest {
   readonly method: 'addnode'
-  readonly params?: any[]
+  readonly params: [string, 'add' | 'remove' | 'onetry']
 }
 
 /**
@@ -21,6 +21,15 @@ export interface AddNodeResponse extends RpcResponse {
  */
 export type AddNodeResult = any
 
-export function AddNode(...params: any[]): AddNodeRequest {
-  return params.length === 0 ? { method: 'addnode' } : { method: 'addnode', params }
+/**
+ * Manages the peer-to-peer connections:
+ * * The `"add"` command queues the specified node for the next available slot;
+ * * The `"remove"` command removes the connection to the specified node;
+ * * The `"onetry"` command adds a connection to the specified node, whether a slot is available or not.
+ * @param host IP address or hostname of the node, optionally including a port number.
+ * @param command Command name.
+ * @returns A JSON-RPC request for the *addnode* command.
+ */
+export function AddNode(host: string, command: 'add' | 'remove' | 'onetry'): AddNodeRequest {
+  return { method: 'addnode', params: [host, command] }
 }
