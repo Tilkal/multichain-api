3c8935eb66302f4810716e7413bdeb389490bc83
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-12 15:07:54 +0200

    Adds unit test for the GetNetworkInfo command.

--- test/Commands/GetNetworkInfo.ts
+++ test/Commands/GetNetworkInfo.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/GetNetworkInfo', function() {
   it(
     'GetNetworkInfo() should return a properly configured JSON-RPC request',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        GetNetworkInfo(),
+        GetNetworkInfo,
+      )
+    },
   )
 })
