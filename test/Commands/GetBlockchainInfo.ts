677ade0d0dc5f05e35bd6b14d8b55034339c3549
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-12 15:04:58 +0200

    Adds unit test for the GetBlockchainInfo command.

--- test/Commands/GetBlockchainInfo.ts
+++ test/Commands/GetBlockchainInfo.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/GetBlockchainInfo', function() {
   it(
     'GetBlockchainInfo() should return a properly configured JSON-RPC request',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        GetBlockchainInfo(),
+        GetBlockchainInfo,
+      )
+    },
   )
 })
