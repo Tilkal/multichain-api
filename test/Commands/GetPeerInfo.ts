030b7a843938aeedd9b821c99ce2c0dbaf9ed871
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-12 15:00:14 +0200

    Adds unit test for the GetPeerInfo command.

--- test/Commands/GetPeerInfo.ts
+++ test/Commands/GetPeerInfo.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/GetPeerInfo', function() {
   it(
     'GetPeerInfo() should return a properly configured JSON-RPC request',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        GetPeerInfo(),
+        GetPeerInfo,
+      )
+    },
   )
 })
