b2b89497ba9e2efcf57c012a8569351a929c612c
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-12 14:58:39 +0200

    Adds unit test for the GetRuntimeParams command.

--- test/Commands/GetRuntimeParams.ts
+++ test/Commands/GetRuntimeParams.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/GetRuntimeParams', function() {
   it(
     'GetRuntimeParams() should return a properly configured JSON-RPC request',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        GetRuntimeParams(),
+        GetRuntimeParams,
+      )
+    },
   )
 })
