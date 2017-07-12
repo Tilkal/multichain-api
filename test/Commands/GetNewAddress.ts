b135006c559be58091a30b29917b02041d5705a5
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-12 15:06:31 +0200

    Adds unit test for the GetNewAddress command.

--- test/Commands/GetNewAddress.ts
+++ test/Commands/GetNewAddress.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/GetNewAddress', function() {
   it(
     'GetNewAddress() should return a properly configured JSON-RPC request',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        GetNewAddress(),
+        GetNewAddress,
+      )
+    },
   )
 })
