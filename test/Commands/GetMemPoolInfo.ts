831bcf17acdbf5542a3ea97f3ae846c04a7283c4
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-12 15:08:54 +0200

    Adds unit test for the GetMemPoolInfo command.

--- test/Commands/GetMemPoolInfo.ts
+++ test/Commands/GetMemPoolInfo.ts
@@ -4,5 +4,12 @@ import { TestCommand } from '../test-helpers'
 describe('/GetMemPoolInfo', function() {
   it(
     'GetMemPoolInfo() should return a properly configured JSON-RPC request',
+    function() {
+      // Signature 1: undefined
+      TestCommand(
+        GetMemPoolInfo(),
+        GetMemPoolInfo,
+      )
+    },
   )
 })
