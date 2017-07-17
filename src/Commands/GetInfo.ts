9e240c8c6957174ea673be67fc2ed3dc7707afac
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-07-17 11:34:51 +0200

    Adds result type for the GetInfo command.

--- src/Commands/GetInfo.ts
+++ src/Commands/GetInfo.ts
@@ -19,8 +19,35 @@ export interface GetInfoResponse extends RpcResponse {
 /**
  * Result of the *getinfo* command.
  */
-export type GetInfoResult = any
-
+export interface GetInfoResult {
+  version: string
+  nodeversion: number
+  protocolversion: number
+  chainname: string
+  description: string
+  protocol: string
+  port: number
+  setupblocks: number
+  nodeaddress: string
+  burnaddress: string
+  incomingpaused: boolean
+  miningpaused: boolean
+  walletversion: number
+  balance: number
+  walletdbversion: number
+  reindex: boolean
+  blocks: number
+  timeoffset: number
+  connections: number
+  proxy: string
+  difficulty: number
+  testnet: boolean
+  keypoololdest: number
+  keypoolsize: number
+  paytxfee: number
+  relayfee: number
+  errors: string
+}
 export function GetInfo(): GetInfoRequest {
   return { method: 'getinfo' }
 }
