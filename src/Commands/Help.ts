d0768ad9639b19b21a58f62e05cbdf9519ae2366
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-08-02 11:45:39 +0200

    Improves type definition for the Help command.

--- src/Commands/Help.ts
+++ src/Commands/Help.ts
@@ -2,25 +2,25 @@ import { RpcRequest } from '../RpcRequest'
 import { RpcResponse } from '../RpcResponse'
 
 /**
- * JSON-RPC request for the *help* command.
+ * JSON-RPC request for the `help` command.
  */
 export interface HelpRequest extends RpcRequest {
   readonly method: 'help'
-  readonly params?: any[]
+  readonly params?: [string]
 }
 
 /**
- * JSON-RPC response for the *help* command.
+ * JSON-RPC response for the `help` command.
  */
 export interface HelpResponse extends RpcResponse {
   readonly result: HelpResult | null
 }
 
 /**
- * Result of the *help* command.
+ * Result of the `help` command.
  */
-export type HelpResult = any
+export type HelpResult = string
 
-export function Help(...params: any[]): HelpRequest {
-  return params.length === 0 ? { method: 'help' } : { method: 'help', params }
+export function Help(command?: string): HelpRequest {
+  return command === undefined ? { method: 'help' } : { method: 'help', params: [command] }
 }
