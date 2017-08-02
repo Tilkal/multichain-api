9c1089327e436f56eff87ac7cd58c7f1bc2bbdc3
Thomas Corbière <thomas.corbiere@tilkal.com>
2017-08-02 11:48:10 +0200

    Adds unit test for the Help command.

--- test/Commands/Help.ts
+++ test/Commands/Help.ts
@@ -4,5 +4,21 @@ import { TestCommand } from '../test-helpers'
 describe('/Help', function() {
   it(
     'Help() should return a properly configured JSON-RPC request',
+    function() {
+      const command = 'help'
+
+      // Signature 1: undefined
+      TestCommand(
+        Help(),
+        Help,
+      )
+
+      // Signature 2: [string]
+      TestCommand(
+        Help(command),
+        Help,
+        [command],
+      )
+    },
   )
 })
